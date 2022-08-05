cca7a78b65b8413c5053288fbc77783e18f96d7d
Ayatullah-khomeni <a.khomeni71@gmail.com>
2022-08-05 23:10:31 +0600

    js

--- multiple-condition.js
+++ multiple-condition.js
@@ -2,3 +2,99 @@ var isGraduated = true;
 if (isGraduated == true) {
     console.log('Eso biye kore feli');
 }
+
+// && sobgulo true hote hobe
+var isGraduated = true;
+var salary = 75000;
+var cars = 1;
+if (isGraduated == true && salary > 50000 && cars >= 1) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+
+// || je kono akta true hote hobe
+var isGraduated = true;
+var salary = 25000;
+if (isGraduated == true || salary > 50000) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+
+var isGraduated = true;
+var salary = 25000;
+var cars = 1;
+if (isGraduated == true || salary > 50000 || cars >= 1) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+var isGraduated = true;
+var salary = 25000;
+var cars = 1;
+if (isGraduated == true && salary > 50000 || cars >= 1) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+
+var isGraduated = true;
+var salary = 25000;
+var cars = 1;
+if (isGraduated == true || salary > 50000 && cars >= 1) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+var isGraduated = true;
+var salary = 75000;
+var cars = 1;
+if (isGraduated == true && salary > 50000 && cars >= 1) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+
+var isGraduated = true;
+var salary = 75000;
+var cars = 1;
+if (isGraduated === true && salary > 50000 && cars >= 1) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+
+var isGraduated = true;
+var salary = 25000;
+var cars = 1;
+if ((isGraduated == true && salary > 50000) || cars >= 1) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
+
+var isGraduated = true;
+var salary = 25000;
+var cars = 1;
+if (isGraduated == true && (salary > 50000 || cars >= 1)) {
+    console.log('Eso biye kore feli');
+}
+else {
+    console.log('tor kopale biya nai');
+}
---
+++ nested-conditions.js
@@ -0,0 +1,20 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if (math == true) {
+    if (geometry == true) {
+        if (straightLine == true) {
+            console.log('ok');
+        }
+        else {
+            console.log('baka pothe cholba na');
+        }
+    }
+    else {
+        console.log('pithagorous hoite parba na')
+    }
+}
+else {
+
+} 
